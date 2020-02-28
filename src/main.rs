a7188c438ac28adaabb8eefae15806e2fbcccc58
tungli <tun@mail.muni.cz>
2020-02-28 17:30:45 +0100

    plotting mod

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use cg_sparse::*;
 
 //use std::fs::File;
 //use std::io::prelude::*;
-//
-//use std::vec::Vec;
 
 use gnuplot::*;
 
@@ -23,7 +21,7 @@ fn main() {
 
     println!("BC set!");
 
-    print!("{}", domain.domain.to_string().as_str());
+    //print!("{}", domain.domain.to_string().as_str());
 
     let (m, bc_rhs) = domain.laplace();
 
@@ -45,74 +43,30 @@ fn main() {
     //buffer.write_all(data);
 
  //   let res = m.cholesky().expect("Decomp. failed").solve(&bc_rhs);
-    let res = conjugate_gradient(&m, &bc_rhs, 1e-6, None);
+    let res = conjugate_gradient(&m, &bc_rhs, 1e-8, None);
 
     println!("Matrix solved!");
-    
+
+    for i in 1..(size-1) {
+        for j in 1..(size-1) {
+            let k = domain.sub2ind_interior((i,j));
+            domain.domain[(i,j)] = res[k];
+        }
+    }
+
     let mut fg = Figure::new();
     
     fg.axes3d()
-    	.set_title("Surface fg3.1", &[])
-    	.surface(res.iter(), size - 2, size - 2, None, &[])
+    	.set_title(format!("Size: {}", size).as_str(), &[])
+    	.surface(domain.domain.transpose().iter(), size, size, Some((0.0, 0.0, 1.0, 1.0)), &[])
     	.set_x_label("X", &[])
     	.set_y_label("Y", &[])
     	.set_z_label("Z", &[])
+        .set_x_range(Fix(0.0), Fix(1.0))
+        .set_y_range(Fix(0.0), Fix(1.0))
         .set_view_map();
     
     fg.show().unwrap();
+    fg.echo_to_file("plot.gpl");
 }
-
-
-
-/*
- *
-    let s = m.to_string();
-    let data = s.as_bytes();
-
-    let mut buffer = File::create("matrix.txt").expect("Exists");
-    buffer.write_all(data);
-
-    let s = rhs.to_string();
-    let data = s.as_bytes();
-
-    let mut buffer = File::create("rhs.txt").expect("Exists");
-    buffer.write_all(data);
- *
-    println!("{:?}", domain);
-    println!("{:?}", domain.x());
-    println!("{:?}", domain.grid().collect::<Vec<(usize, usize)>>());
- *
-
-    let a = Linspace::new(-1.0, 21.0, 30);
-    println!("dx: {}", a.dx());
-    println!("{:?}", a.gen());
-    println!("{}", a.gen().len());
-
-    let (nx, ny) = (3, 3);
-    let mut mx = DMatrix::identity(nx, nx);
-    let mut my = DMatrix::identity(ny, ny);
-
-    for i in 0..nx {
-        mx[(i,i)] = 2;
-    }
-    for i in 0..(nx-1) {
-        mx[(i+1, i)] = -1;
-        mx[(i, i+1)] = -1;
-    }
-    for i in 0..ny {
-        my[(i,i)] = 2;
-    }
-    for i in 0..(ny-1) {
-        my[(i+1, i)] = -1;
-        my[(i, i+1)] = -1;
-    }
-    let m = mx.kronecker(&my);
-    for i in m.row_iter() {
-        for j in i.column_iter() {
-            print!("{:?} ", j[0]);
-        }
-        println!("");
-    }
-    println!("{}", 3.21.cos());
-*/
 
