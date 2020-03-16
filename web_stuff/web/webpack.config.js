8a0c99d3c88e7eb4ad59f73be559c8096e8a1a95
tungli <tun@mail.muni.cz>
2020-03-16 19:20:17 +0100

    publish mol

--- web_stuff/web/webpack.config.js
+++ web_stuff/web/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 module.exports = {
-  entry: "./index.js",
+  entry: {
+    fd_laplace: "./fd_laplace.js",
+    mol_diffusion: "./mol_diffusion.js"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "[name].bundle.js"
   },
   mode: "development",
   module: {
